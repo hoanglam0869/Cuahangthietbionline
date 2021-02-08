f9586f18d1f0ead9b5f986a57b91a081a1edd14a
Admins <hoanglam0869@gmail.com>
2021-02-09 06:18:58 +0700

    Cửa Hàng Thiết Bị Online

--- app/src/main/java/com/hoanglam0869/cuahangthietbionline/ultil/Server.java
+++ app/src/main/java/com/hoanglam0869/cuahangthietbionline/ultil/Server.java
@@ -3,7 +3,7 @@ package com.hoanglam0869.cuahangthietbionline.ultil;
 import com.hoanglam0869.cuahangthietbionline.model.Sanpham;
 
 public class Server {
-    public static String localhost = "192.168.1.8";
+    public static String localhost = "hoanglam0869.000webhostapp.com";
     public static String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
     public static String Duongdansanphammoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
     public static String Duongdandienthoai = "http://" + localhost + "/server/getsanpham.php?page=";
